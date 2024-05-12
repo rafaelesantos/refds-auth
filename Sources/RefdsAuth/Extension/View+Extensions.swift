010ec1548bc75ca90fa16a3dbbb6adddc7014ea9
Rafael Escaleira <rafaelescaleira@icloud.com>
2024-05-12 19:30:12 -0400

    Update View+Extensions.swift

--- Sources/RefdsAuth/Extension/View+Extensions.swift
+++ Sources/RefdsAuth/Extension/View+Extensions.swift
@@ -3,11 +3,13 @@ import SwiftUI
 public extension View {
     func refdsAuth(
         autheticated: Binding<Bool>,
+        applicationIcon: Image,
         isAutomaticRequest: Bool = true
     ) -> some View {
         self.modifier(
             RefdsRequestBiometryViewModifier(
                 isAuthenticated: autheticated,
+                applicationIcon: applicationIcon,
                 isAutomaticRequest: isAutomaticRequest
             )
         )
